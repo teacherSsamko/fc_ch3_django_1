fe3a48f69fbd78aa06bd582df82e9b53333629c4
ssamko <teacher.ssamko@gmail.com>
2020-02-24 19:59:54 +0900

    login page

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Fcuser
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        res_data = {}
+        if not (username and password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
+        else:
+            fcuser = Fcuser.objects.get(username=username)
+            if check_password(password, fcuser.password):
+                # login process
+                # Session
+                # Redirect
+                return redirect('/')
+
+            else:
+                res_data['error'] = '비밀번호가 틀립니다.'
+
+        return render(request, 'login.html', res_data)
 
 
 def register(request):
