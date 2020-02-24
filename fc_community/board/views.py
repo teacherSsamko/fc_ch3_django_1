40b5f915720a294ffcee168a0c0b1c725330fe7c
ssamko <teacher.ssamko@gmail.com>
2020-02-25 00:23:20 +0900

    update paginator

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
+from django.core.paginator import Paginator
 from .models import Board
 from .forms import BoardForm
 from fcuser.models import Fcuser
@@ -39,5 +40,10 @@ def board_write(request):
 
 
 def board_list(request):
-    boards = Board.objects.all().order_by('-id')
+    all_boards = Board.objects.all().order_by('-id')
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(all_boards, 3)
+
+    boards = paginator.get_page(page)
+
     return render(request, 'board_list.html', {'boards': boards})
