412d7e34823cd1a04949cf7c9635512222c29428
ssamko <teacher.ssamko@gmail.com>
2020-02-24 14:39:09 +0900

    fc_community project start

---
+++ fcdjango_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ssamko/Documents/python_django/fcdjango_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ fcuser/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FcUserConfig(AppConfig):
+    name = 'fc_user'
