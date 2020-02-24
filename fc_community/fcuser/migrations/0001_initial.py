4b8ea1f9206d6d8a1171a0f16431b5426d670818
ssamko <teacher.ssamko@gmail.com>
2020-02-24 15:55:18 +0900

    fcuser admin setup

--- fc_community/fcuser/migrations/0001_initial.py
+++ fc_community/fcuser/migrations/0001_initial.py
@@ -14,10 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Fcuser',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('username', models.CharField(max_length=64, verbose_name='사용자명')),
                 ('password', models.CharField(max_length=64, verbose_name='비밀번호')),
-                ('registered_dttm', models.DateTimeField(auto_now_add=True, verbose_name='등록시간')),
+                ('registered_dttm', models.DateTimeField(
+                    auto_now_add=True, verbose_name='등록시간')),
             ],
             options={
                 'db_table': 'fastcampus_fcuser',
--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -4,10 +4,15 @@ from django.db import models
 
 
 class Fcuser(models.Model):
-    username = models.CharField(max_length=64, verbose_name='사용자명')
+    username = models.CharField(max_length=32, verbose_name='사용자명')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(
         auto_now_add=True, verbose_name='등록시간')
 
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'fastcampus_fcuser'
+        verbose_name = "패스트 캠퍼스 사용자"
+        verbose_name_plural = '패스트 캠퍼스 사용자'
