b84b5f7d6a463be8eaea0219b2c0a98160be3940
ssamko <teacher.ssamko@gmail.com>
2020-02-24 23:43:03 +0900

    board_detail page

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Board
+from .forms import BoardForm
+from fcuser.models import Fcuser
 
 # Create your views here.
+
+
+def board_detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'board_detail.html', {'board': board})
+
+
+def board_write(request):
+    if request.method == 'POST':
+        form = BoardForm(request.POST)
+        if form.is_valid():
+            user_id = request.session.get('user')
+            fcuser = Fcuser.objects.get(pk=user_id)
+            board = Board()
+            board.title = form.cleaned_data['title']
+            board.contents = form.cleaned_data['contents']
+            board.writer = fcuser
+            board.save()
+
+            return redirect('/board/list/')
+    else:
+        form = BoardForm()
+
+    return render(request, 'board_write.html', {'form': form})
 
 
 def board_list(request):
